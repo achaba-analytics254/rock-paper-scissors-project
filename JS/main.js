7cb3106c944e2bb29e0391a296012b7dde3a6faf
achaba-analytics254 <djangozent@gmail.com>
2026-02-13 22:01:32 +0300

    Update: DOM-ui

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,41 @@
 // rock, paper, scissors in javascript
 
-// Logic to get computer choice
+// div elements
+const buttonContainer = document.getElementById('buttonContainer');
+const displayChoices =document.getElementById('displayChoices');
+const scoreResult = document.getElementById('scoreResult');
+const finalScore = document.getElementById('finalScore');
+const resultDeclaration = document.getElementById('resultDeclaration');
 
+// create buttons
+const rockBtn = document.createElement('button');
+rockBtn.textContent = 'Rock';
+rockBtn.id = 'rock'
+const paperBtn = document.createElement('button');
+paperBtn.textContent = 'Paper';
+paperBtn.id = 'paper'
+const scissorsBtn = document.createElement('button');
+scissorsBtn.textContent = 'Scissors';
+scissorsBtn.id = 'scissors'
+
+// append children to button container
+buttonContainer.appendChild(rockBtn);
+buttonContainer.appendChild(paperBtn);
+buttonContainer.appendChild(scissorsBtn);
+
+// create p text
+const displayChoicesText = document.createElement('p');
+const scoreResultText = document.createElement('p');
+const finalScoreText = document.createElement('p');
+const resultDeclarationText = document.createElement('p');
+
+// append p to div containers
+displayChoices.appendChild(displayChoicesText);
+scoreResult.appendChild(scoreResultText);
+finalScore.appendChild(finalScoreText);
+resultDeclaration.appendChild(resultDeclarationText);
+
+// Logic to get computer choice
 const getComputerChoice = () => {
     let choices = Math.floor(Math.random() * 3)
 
@@ -15,11 +49,18 @@ const getComputerChoice = () => {
     }
 }
 
-// Logic to get human choice to lowercase (case sensitive)
+// Logic to get human choice
+buttonContainer.addEventListener('click', (e) => {
+    const target = e.target.closest('button');
 
-const getHumanChoice = () => {
-    return prompt('Enter rock, paper or scissors to play').toLowerCase()
-}
+    if (!target) return;
+
+   const humanChoice = target.id;
+   const computerChoice = getComputerChoice();
+
+    playRound(humanChoice, computerChoice);
+})
+
 
 // Declaring players scores and initializing to 0.
 
@@ -29,45 +70,55 @@ let computerScore = 0;
 // Logic to play a single round
 
 const playRound = (humanChoice, computerChoice) => {
-    console.log(`Your choice: ${humanChoice}`);
-    console.log(`Computer Choice: ${computerChoice}`);
+    displayChoicesText.textContent = `Your choice: ${humanChoice} | Computer Choice: ${computerChoice}`;
     
     // compare choices
 
     if (humanChoice === computerChoice){
-        console.log('It\'s a tie!');
+        scoreResultText.textContent = 'It\'s a tie!'
     } else if (
         (humanChoice === 'rock' && computerChoice === 'scissors') ||
         (humanChoice === 'scissors' && computerChoice === 'paper') ||
         (humanChoice === 'paper' && computerChoice === 'rock')
     ) {
-        console.log(`You win this round: ${humanChoice} beats ${computerChoice}`);
+        scoreResultText.textContent = `You win this round: ${humanChoice} beats ${computerChoice}`;
         humanScore++;
     } else {
-        console.log(`Computer wins this round: ${computerChoice} beats ${humanChoice}`);
+        scoreResultText.textContent = `Computer wins this round: ${computerChoice} beats ${humanChoice}`;
         computerScore++;
     }
-    console.log(`Your score: ${humanScore} | Computer score: ${computerScore}`);
-}
+    finalScoreText.textContent = `Your score: ${humanScore} | Computer score: ${computerScore}`;
 
-const playGame = () => {
-    // play 5 rounds
-    for (let round = 0; round < 5; round++){
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
+    if (humanScore === 5 || computerScore === 5){
+        if (humanScore === 5){
+            resultDeclarationText.textContent = 'Congratulations! You\'ve won the Game';
+        } else {
+            resultDeclarationText.textContent = 'Computer has won the Game';
+        }
+                // buttons disabled
 
-    // Results
-
-console.log('Results:')
-    if (humanScore > computerScore){
-        console.log('Congratulations! You\'ve won the game');
-    } else if (computerScore > humanScore){
-        console.log('Computer has won the game!');
-    } else {
-        console.log('It\'s a Draw!');
+    const buttons = buttonContainer.querySelectorAll('button');
+    buttons.forEach(btn => btn.disabled = true);
     }
 }
 
-playGame()
+// reset button
+const resetButton = document.getElementById('resetButton');
+const resetBtn = document.createElement('button');
+resetBtn.textContent = 'Reset';
+resetButton.appendChild(resetBtn);
+
+resetBtn.addEventListener('click', () => {
+    humanScore = '';
+    computerScore = '';
+    resultDeclarationText.textContent = '';
+    finalScoreText.textContent = '';
+    scoreResultText.textContent = '';
+    displayChoicesText.textContent = '';
+
+    // buttons enabled
+
+    const buttons = buttonContainer.querySelectorAll('button');
+    buttons.forEach(btn => btn.disabled = false);
+
+});
